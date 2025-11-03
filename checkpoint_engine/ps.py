ced8a28b8ee124b2bdae035b238f62a3e02b6679
cuixiaojin <c00855547@china.huawei.com>
2025-11-03 10:51:21 +0800

    P2P mode adaptation

--- checkpoint_engine/ps.py
+++ checkpoint_engine/ps.py
@@ -671,14 +671,19 @@ class P2PStore:
         self.rank = int(os.getenv("RANK"))
         gpu_count = device_manager.device_module.device_count()
         local_rank = self.rank % gpu_count
-        self.device = _get_my_rdma_device(local_rank, gpu_count, _get_rdma_devices())
+        if device_manager.device_type == "cuda":
+            self.device = _get_my_rdma_device(local_rank, gpu_count, _get_rdma_devices())
+            protocol = "rdma"
+        elif device_manager.device_type == "npu":
+            self.device = ""
+            protocol = "ascend_direct"
         self.ip = get_ip()
 
         # we will start at most 8 ps processes, so we use 8 retries to avoid port conflicts in extreme cases
         retry_count = 8
         for i in range(retry_count):
             self.engine = TransferEngine()
-            ret = self.engine.initialize(self.ip, "P2PHANDSHAKE", "rdma", self.device)
+            ret = self.engine.initialize(self.ip, "P2PHANDSHAKE", protocol, self.device)
             if ret == 0:
                 break
             # sleep 0.5 ~ 2.0s, to avoid port conflicts when two processes retry at the same time
@@ -776,14 +781,15 @@ class ParameterServer:
         self._memory_pool: dict[str, list[MemoryBuffer]] = {}
         # dict key is owner_rank, value is a bucket metas list in owner_rank
         self._current_global_parameter_metas: dict[int, MemoryBufferMetaList] = {}
+        # NPU transfer engine initialization requires prior set_device.
+        device_index = self._local_rank
+        self.device_manager.device_module.set_device(device_index)
         try:
             self._p2p_store = P2PStore(self.device_manager)
         except ImportError as e:
             logger.warning(f"[rank{self._rank}] fail to initialize p2p store due to {e}")
             self._p2p_store = None
 
-        device_index = self._local_rank
-        self.device_manager.device_module.set_device(device_index)
         self._device_uuid = _get_physical_gpu_id(self.device_manager, device_index)
         self._rdma_device = None if self._p2p_store is None else self._p2p_store.device
 
@@ -1123,7 +1129,7 @@ class ParameterServer:
             master_addr, master_port, len(ranks), is_master=rank == 0, timeout=timeout
         )
         dist.init_process_group(
-            backend="nccl", world_size=len(ranks), rank=rank, timeout=timeout, store=store
+            backend=self.device_manager.backend, world_size=len(ranks), rank=rank, timeout=timeout, store=store
         )
 
     def _get_addr_ptrs(self, owner_rank: int) -> tuple[str, list[tuple[int, int]]]:
